86f13af7dd8fc340a4bfa3cc825ffe8160f3bb5c
Sumukha Rao H <sumukharaoh05@gmail.com>
2025-01-19 09:36:57 +0530

    render test

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,13 +1,20 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+
+// Check if running in a Render environment
+const isRender = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+const serviceAccount = isRender
+  ? JSON.parse(
+      Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+    )
+  : require("../chat-app-8f477-firebase-adminsdk-i4r5u-f9a91f2fe4.json"); // Local JSON file for development
 
 // Initialize Firebase Admin SDK
-// Replace this with the path to your service account key JSON file
-const serviceAccount = require('../chat-app-8f477-firebase-adminsdk-i4r5u-f9a91f2fe4.json');
-
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount), // Use service account for authentication
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 module.exports = admin;
+
